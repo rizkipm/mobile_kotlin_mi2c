c5fdce8f93cfa105b43a1386a31153baba4412b7
Rizki Pm <udarizki2020@gmail.com>
2024-08-24 11:55:32 +0700

    update page

---
+++ app/src/main/java/com/rizki/mobileappmi2c/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.rizki.mobileappmi2c
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit var btnClick : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        btnClick = findViewById(R.id.btnClickHere)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnClick.setOnClickListener(){
+            Toast.makeText(this, "Anda klik button click here!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
